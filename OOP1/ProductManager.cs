ad7d39a18be9ce43851ffdf5d3bae1b87e2c9e99
ismail yarar <ismyarar@gmail.com>
2021-01-22 20:34:08 +0300

    16 Ocak

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //CRUD operasyonları
+        public void Add(Product product)
+        {
+            product.ProductName = "Kamera";
+        }
+
+        public void Test(int sayi)
+        {
+            sayi = 25;
+        }
+
+        
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 100;
+            product1.UnitsInStock = 25;
+
+            //bu şekilde de tanımlama yapabiliriz
+            Product product2 = new Product { Id = 2, CategoryId = 5, ProductName = "Lamba", UnitPrice = 125,UnitsInStock=14 };
+            //PascalCase    //camelCase
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            //Referans tip olduğu için Kamera yazdırdı
+            Console.WriteLine(product1.ProductName);
+
+            //değer tip olduğu için 10 yazdırdı
+            int sayi = 10;
+            productManager.Test(sayi);
+            Console.WriteLine(sayi);
+            
+        }
+    }
+}
